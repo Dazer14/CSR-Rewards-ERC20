f8988107d3788f3d7b2e9c748a492479e98b1379
Dazer <bbn44@proton.me>
2023-08-08 23:08:11 -0700

    Create2 example updated

--- src/examples/eligibility/create2/Create2EligibilityFactory.sol
+++ src/examples/eligibility/create2/Create2EligibilityFactory.sol
@@ -3,34 +3,34 @@ pragma solidity ^0.8.0;
 
 import "lib/openzeppelin-contracts/contracts/utils/Create2.sol";
 // import "@openzeppelin/contracts/utils/Create2.sol";
-import "./Create2EligibleContract.sol";
+import "./Create2EligibleExample.sol";
 
 /// @dev Have to send some tokens to this contract
 /// Use Create2 to calculate contract deploy address
 /// Tokens are transferred there to make the eventual contract address reward eligible
 /// Create2 used to deploy 
 contract Create2EligibilityFactory {
-    IERC20 immutable _csrRewardsToken;
+    IERC20 public immutable csrRewardsToken;
 
-    constructor(address csrRewardsToken) {
-        _csrRewardsToken = IERC20(csrRewardsToken);
+    constructor(address _csrRewardsToken) {
+        csrRewardsToken = IERC20(_csrRewardsToken);
     }
 
     function makeAddressEligibleAndDeploy(bytes32 salt) external {
         bytes memory bytecode = abi.encodePacked(
-            type(Create2EligibleContract).creationCode, 
-            abi.encode(address(_csrRewardsToken)),
+            type(Create2EligibleExample).creationCode, 
+            abi.encode(address(csrRewardsToken)),
             abi.encode(address(msg.sender)) // Owner of deployed contract
         );
-        require(_csrRewardsToken.balanceOf(address(this)) > 0, "No tokens to send, transfer some here");
+        require(csrRewardsToken.balanceOf(address(this)) > 0, "No tokens to send, transfer some here");
         address dest = Create2.computeAddress(salt, keccak256(bytecode));
-        _csrRewardsToken.transfer(dest, 1); // Made eligible
+        csrRewardsToken.transfer(dest, 1); // Made eligible
         Create2.deploy(0, salt, bytecode);
     }
 
     function retrieveTokens(address receiver) external {
-        uint balance = _csrRewardsToken.balanceOf(address(this));
-        _csrRewardsToken.transfer(receiver, balance);
+        uint balance = csrRewardsToken.balanceOf(address(this));
+        csrRewardsToken.transfer(receiver, balance);
     }
 
 }
---
+++ src/examples/eligibility/create2/Create2EligibleExample.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+import "../RewardEligibleContract.sol";
+
+/// @dev This would be deployed by the Create2 factory
+contract Create2EligibleExample is RewardEligibleContract, Ownable {
+    constructor(
+        address _csrRewardsToken, 
+        address _owner
+    ) RewardEligibleContract(_csrRewardsToken) {
+        _transferOwnership(_owner);
+    }
+
+    function getReward(address receiver) public override onlyOwner {
+        super.getReward(receiver);
+    }
+
+    function retrieveTokens(address receiver) public override onlyOwner {
+        super.retrieveTokens(receiver);
+    }
+
+}
