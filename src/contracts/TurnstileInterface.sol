6cef55bc3c9f8337de19545bca73bc23ddd71e9a
Dazer <bbn44@proton.me>
2023-08-20 22:18:48 -0700

    Interface

--- src/contracts/TurnstileInterface.sol
+++ src/contracts/TurnstileInterface.sol
@@ -5,5 +5,5 @@ interface TurnstileInterface {
     function register(address _recipient) external returns (uint256 tokenId);
     function assign(uint256 _tokenId) external;
     function balances(uint256 _tokenId) external view returns (uint256 feesEarned);
-    function withdraw(uint256 _tokenId, address payable _recipient, uint256 _amount) external returns (uint256 _amount);
+    function withdraw(uint256 _tokenId, address payable _recipient, uint256 _amount) external returns (uint256 amount);
 }
